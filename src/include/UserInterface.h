e96b529e6d22f0a4cc1b01fd4e6571e62f476dfd
Li Lingfeng <lilingfeng0408@outlook.com>
2024-09-01 21:57:58 +0800

    Update UserInterface class destructor

--- src/include/UserInterface.h
+++ src/include/UserInterface.h
@@ -10,6 +10,7 @@
 class UserInterface
 {
 public:
+	virtual ~UserInterface() = default;
 	virtual void handleEvent(const sf::Event &event) = 0;
 	virtual void update() = 0;
 	virtual void render(sf::RenderWindow &window) = 0;
