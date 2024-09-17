48bcd47019b3b26b4e60a4ef0cc9898df605895d
Li Lingfeng <lilingfeng0408@outlook.com>
2024-09-17 18:04:03 +0800

    Refactor asset path handling in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,14 @@
 int main(int argc, char *argv[])
 {
 	std::clog << "Starting game at " << argv[0] << std::endl;
-	auto path = std::filesystem::current_path();
-	if (std::filesystem::current_path().string().find("bin" || "build" || "Debug" || "Release") != std::string::npos)
-	{
-		path = path.parent_path();
-	}
+	auto path = std::filesystem::path(argv[0]).parent_path();
+	std::clog << "Path: " << path.string() << std::endl;
+	// find "build", "bin", "Debug", "Release" in path, if exist then parent_path();
+    while (path.filename() == "build" || path.filename() == "bin" || path.filename() == "Debug" || path.filename() == "Release")
+    {
+        path = path.parent_path();
+        std::clog << "Path: " << path.string() << std::endl;
+    }
 	auto game = std::make_shared<Game>();
 	game->setAssetPrefix(path.string() + "/assets/");
 	game->init();
